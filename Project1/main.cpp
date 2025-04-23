3ec45f9d5f4f842ac3486bfe2f8c90e9cf13c81d
JManBruh <53465597+JManBruh@users.noreply.github.com>
2025-04-22 19:40:05 -0700

    restart
    removed batching class

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,11 +1,30 @@
-#include "draw.hpp"
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/OpenGL.hpp>
+#include <vector>
+
+using namespace sf;
+using std::vector;
 
 int main()
 {
-    SFMLDraw render;
+	Texture guy("img/fisherman.png", false, IntRect({ 10, 10 }, { 16, 16 }));
+	Sprite sprguy(guy);
+	sprguy.setPosition({10.f, 50.f});
+	sprguy.setRotation(sf::degrees(90));
+	sprguy.setScale({ 0.5f, 2.f });
 
-    render.add("img/grass.png");
-    render.add("img/fisherman.png");
-    render.draw();
-    
+	RenderWindow window(sf::VideoMode({ 1600, 900 }), "PA 9: FISHING");
+	while (window.isOpen())
+	{
+		while (const std::optional event = window.pollEvent())
+		{
+			if (event->is<Event::Closed>())
+				window.close();
+		}
+		window.clear();
+		window.draw(sprguy);
+		window.display();
+	}
+
 }
